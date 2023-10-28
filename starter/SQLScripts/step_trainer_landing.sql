b9d6cf2bff4d7a839bb3c0a69dfcc515c89babed
NajdAlkhalaf <99121151+NajdAlkhalaf@users.noreply.github.com>
2023-10-28 14:42:39 +0300

    Add files via upload

---
+++ starter/SQLScripts/step_trainer_landing.sql
@@ -0,0 +1,14 @@
+CREATE EXTERNAL TABLE `step_trainer_landing`(
+  `sensorreadingtime` bigint, 
+  `serialnumber` string, 
+  `distancefromobject` int)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'ignore.malformed.json' = 'FALSE',
+  'dots.in.keys' = 'FALSE',
+  'case.insensitive' = 'TRUE',
+  'mapping' = 'TRUE'
+)
+STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://stediproject/customer/landing/'
+TBLPROPERTIES ('classification' = 'json');
